a30b7caa5e127b30f0bf0fc953a50eb5f996a876
Ian Yoo <yooian03@gmail.com>
2025-04-19 16:54:40 -0700

    Fix import globals.scss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css';
+import './globals.scss';
 import { SupabaseProvider } from './providers';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
