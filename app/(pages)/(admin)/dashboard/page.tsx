01239c5a304da2535b5c99a777c8bd9893edb1ac
C3viche <28328104+C3viche@users.noreply.github.com>
2025-04-19 16:17:15 -0700

    dashboard start + card globals

--- app/(pages)/(admin)/dashboard/page.tsx
+++ app/(pages)/(admin)/dashboard/page.tsx
@@ -1,5 +1,11 @@
+import styles from './page.module.scss';
+
 const Dashboard = () => {
-    
+    return (
+        <main className={styles.page}>
+            <h1>Dashboard</h1>
+        </main>
+    );
 }
 
 export default Dashboard;
