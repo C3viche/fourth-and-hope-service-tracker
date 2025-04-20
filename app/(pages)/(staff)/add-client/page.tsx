b11a534323662c19c8a8aaabc4d9fa23fec783b3
Ian Yoo <yooian03@gmail.com>
2025-04-19 23:14:25 -0700

    Connect to client database with GET API

--- app/(pages)/(staff)/add-client/page.tsx
+++ app/(pages)/(staff)/add-client/page.tsx
@@ -2,67 +2,44 @@
 
 import AddUsersLayout from '../../../(components)/add-client/AddUsersLayout';
 import UserCard from '../../../(components)/add-client/UserCard';
+import { Client } from '@/lib/supabase';
 
-const dummyUsers = [
-    {
-      id: 1,
-      name: "Jane Doe",
-      pronunciation: "Jay-n Doh",
-      age_group: "18-39",
-      sex: "Female",
-      ethnicity: "Hispanic",
-      language: "Spanish",
-      stay_type: "overnight",
-      notes: "Requires medication every morning.",
-      date_added: new Date("2024-12-12"),
-    },
-    {
-        id: 2,
-        name: "Joseph Doe",
-        pronunciation: "Jay-n Doh",
-        age_group: "18-39",
-        sex: "Female",
-        ethnicity: "Hispanic",
-        language: "Spanish",
-        stay_type: "overnight",
-        notes: "Requires medication every morning.",
-        date_added: new Date("2024-12-12"),
-      },{
-        id: 3,
-        name: "Janie Doe",
-        pronunciation: "Jay-n Doh",
-        age_group: "18-39",
-        sex: "Female",
-        ethnicity: "Hispanic",
-        language: "Spanish",
-        stay_type: "overnight",
-        notes: "Requires medication every morning.",
-        date_added: new Date("2024-12-12"),
-      },{
-        id: 4,
-        name: "Jacob Doe",
-        pronunciation: "Jay-n Doh",
-        age_group: "18-39",
-        sex: "Female",
-        ethnicity: "Hispanic",
-        language: "Spanish",
-        stay_type: "overnight",
-        notes: "Requires medication every morning.",
-        date_added: new Date("2024-12-12"),
-      },
-  ];
+import { useEffect, useState } from 'react';
+
 
 export default function AddUsersPage() {
+
+  const [users, setUsers] = useState<Client[]>([]); // of type Client
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchClients = async () => {
+      try {
+        const res = await fetch('/api/client');
+        const data = await res.json();
+        setUsers(data);
+        console.log("FETCHED! ", data);
+      } catch (err) {
+        console.error('Failed to fetch clients:', err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchClients();
+  }, []);
+
   return (
-    // <div>
-    // 
     <AddUsersLayout>
-    <UserCard isAddButton />
-    {dummyUsers.map((user) => (
-        <UserCard key={user.id} user={user} />
-    ))}
+      <UserCard isAddButton />
+      {loading ? (
+        <p>Loading clients...</p>
+      ) : (
+        users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))
+      )}
     </AddUsersLayout>
-    // </div>
   );
 }
 
